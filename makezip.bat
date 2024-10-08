f43139705b0b3bc3f020fa5a96339286ab0ccc15
donk7413 <32650379+donk7413@users.noreply.github.com>
2024-10-08 12:06:52 +0200

    better makezip

--- makezip.bat
+++ makezip.bat
@@ -1,4 +1,14 @@
-del "datapack.7z"
+@echo off
+rem Get the folder path of the executed batch file
+set "folderPath=%~dp0"
+rem Get the folder name
+for %%F in ("%folderPath:~0,-1%") do set "folderName=%%~nF"
+rem Add .7zip at the end of the folder name
+set "archiveName=%folderName%.7z"
 
-7z.exe a "datapack.7z" .\bin
-7z.exe a "datapack.7z" .\mods
+rem Delete any existing archive with the same name
+if exist "%archiveName%" del "%archiveName%"
+
+rem Run 7z.exe to create the archive
+7z.exe a "%archiveName%" .\bin
+7z.exe a "%archiveName%" .\r6
