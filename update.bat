fe7ae5a8159ba3da6841ac8cdf1ec09660cc098a
donk7413 <32650379+donk7413@users.noreply.github.com>
2024-10-11 09:58:36 +0200

    Create update.bat

---
+++ update.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal
+
+:: Set the source and destination folders
+set "source_folder=H:\GitHub\cyberscript-mod-template\bin\x64\plugins\cyber_engine_tweaks\mods\mytemplate\libs"
+set "destination_folder=H:\GitHub\datapack\datapack"
+
+:: Set the file name to check and replace (without path)
+set "file_name=cyberlibs.lua"
+
+:: Check and replace matching files in destination subfolders
+for /r "%destination_folder%" %%f in (*) do (
+    if /i "%%~nxf"=="%file_name%" (
+        if exist "%source_folder%\%file_name%" (
+            echo Replacing "%%f" with "%source_folder%\%file_name%"
+            copy /y "%source_folder%\%file_name%" "%%f"
+        )
+    )
+)
+
+echo File check and replace completed for %file_name%.
+
+
+set "source_folder=H:\GitHub\cyberscript-mod-template\bin\x64\plugins\cyber_engine_tweaks\mods\mytemplate"
+
+:: Set the file name to check and replace (without path)
+set "file_name=init.lua"
+
+:: Check and replace matching files in destination subfolders
+for /r "%destination_folder%" %%f in (*) do (
+    if /i "%%~nxf"=="%file_name%" (
+        if exist "%source_folder%\%file_name%" (
+            echo Replacing "%%f" with "%source_folder%\%file_name%"
+            copy /y "%source_folder%\%file_name%" "%%f"
+        )
+    )
+)
+
+echo File check and replace completed for %file_name%.
+pause
