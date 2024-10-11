2d17a46972926c2c2521a42dbae380365b829f3d
donk7413 <32650379+donk7413@users.noreply.github.com>
2024-10-11 14:57:35 +0200

    update mod folder

--- update.bat
+++ update.bat
@@ -37,4 +37,25 @@ for /r "%destination_folder%" %%f in (*) do (
 )
 
 echo File check and replace completed for %file_name%.
+
+
+
+:: Set the source and destination folders
+set "source_folder=H:\GitHub\cyberscript-mod-template\bin\x64\plugins\cyber_engine_tweaks\mods\mytemplate\libs"
+set "destination_folder=G:\SteamLibrary\steamapps\common\Cyberpunk 2077\bin\x64\plugins\cyber_engine_tweaks\mods"
+
+:: Set the file name to check and replace (without path)
+set "file_name=cyberlibs.lua"
+
+:: Check and replace matching files in destination subfolders
+for /r "%destination_folder%" %%f in (*) do (
+    if /i "%%~nxf"=="%file_name%" (
+        if exist "%source_folder%\%file_name%" (
+            echo Replacing "%%f" with "%source_folder%\%file_name%"
+            copy /y "%source_folder%\%file_name%" "%%f"
+        )
+    )
+)
+
+echo File check and replace in mods folder completed for %file_name%.
 pause
